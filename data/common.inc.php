457600fcb379426b59ffc8f5ea2e4e65a1f67b5c
xuzhiheng <916813946@qq.com>
2020-04-11 15:21:30 +0800

    第一次提交

---
+++ data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = 'hdm643985576.my3w.com';
+$cfg_dbname = 'hdm643985576_db';
+$cfg_dbuser = 'hdm643985576';
+$cfg_dbpwd = 'XZHlove1314520';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
